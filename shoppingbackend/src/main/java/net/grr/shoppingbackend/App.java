2af238b5eaddec428d3a44ce5ede300c767f35d3
Raghuram G <raghuramg1992@gmail.com>
2018-08-14 12:59:38 +0530

    My Project Setup Commited successfully

---
+++ shoppingbackend/src/main/java/net/grr/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.grr.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
