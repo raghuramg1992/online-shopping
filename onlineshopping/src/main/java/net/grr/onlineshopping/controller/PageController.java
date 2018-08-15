ef03610cbf43b59f00de5d73d57349dac87b4de9
Raghuram G <raghuramg1992@gmail.com>
2018-08-15 09:10:50 +0530

    annotation related

--- onlineshopping/src/main/java/net/grr/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/grr/onlineshopping/controller/PageController.java
@@ -2,6 +2,7 @@ package net.grr.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,6 +12,16 @@ public class PageController {
 	public ModelAndView index() {
 		ModelAndView mv=new ModelAndView("page");
 		mv.addObject("greeting","Welcome to Spring MVC");
+		return mv;
+		}
+	@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam(value= "greeting",required=false) String greeting) {
+		if(greeting==null) {
+			greeting="Hello There";
+		}
+		
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting",greeting);
 		return mv;
 	}
 }
